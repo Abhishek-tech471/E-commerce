18139cbdd3db77db3c872e88038b6a72b66acfc1
Abhishek-tech471 <abhishek.yadav_cs.bct23@gla.ac.in>
2025-09-06 22:02:47 +0530

    Db connected

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const { log } = require('console');
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopping')
+.then(()=>{
+    console.log("Database connected Successfully");
+    
+})
+.catch((err)=>{
+    console.log("Error found:", err);
+    
+}) 
 
 app.set('view engine', 'ejs');
 app.set('view' ,path.join(__dirname,'views'));

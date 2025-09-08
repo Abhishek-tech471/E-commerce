846400a57684c628a6080c0aca262bec7c98debd
Abhishek-tech471 <abhishek.yadav_cs.bct23@gla.ac.in>
2025-09-08 22:15:31 +0530

    new product done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const { log } = require('console');
 const seedDB = require('./seed');
 const ProductRoutes = require('./routes/product');
+const ejsMate = require('ejs-mate');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/shopping')
@@ -17,9 +18,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopping')
     
 }) 
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views' ,path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }))
 
 // seeding DB
 // seedDB();
--- routes/product.js
+++ routes/product.js
@@ -7,5 +7,31 @@ router.get('/products', async (req,res)=>{
     res.render('products/index',{products});
 })
 
+router.get('/product/new',(req,res)=>{
+    res.render('products/new');
+})
+
+router.post('/products',async(req,res)=>{
+    // let data = req.params;
+    let product = req.body;
+    // let a = await Product.find(product);
+    // console.log(a);
+    // if(a==product){
+    //     console.log('product already available');
+        
+    // }
+    // else{
+    await Product.insertOne(product)
+    .then(()=>{
+        log('data added successfully');
+    })
+
+// }
+    let products =await Product.find({});
+    res.redirect('/products')
+
+  
+})
+
 
 module.exports = router;
